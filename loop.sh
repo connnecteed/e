2d3a3577b553e6117d057912d5170a29628ba98e
connnecteed <23ns7zxbw@gmail.com>
2025-08-28 16:11:01 +0300

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 pip3 install --quiet psutil requests
-curl -s -L -o loop.py https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/connnecteed/e/refs/heads/main/loop.py
 python3 loop.py
